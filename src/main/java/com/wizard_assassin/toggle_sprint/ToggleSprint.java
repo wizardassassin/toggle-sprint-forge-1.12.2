1ae15ff2f51d6a4b2e5d6149827f20d4fb624f4c
Victor Chen <60045243+wizardassassin@users.noreply.github.com>
2024-02-12 14:05:01 -0800

    Added logger

--- src/main/java/com/wizard_assassin/toggle_sprint/ToggleSprint.java
+++ src/main/java/com/wizard_assassin/toggle_sprint/ToggleSprint.java
@@ -1,14 +1,24 @@
 package com.wizard_assassin.toggle_sprint;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ToggleSprint.MOD_ID)
 public class ToggleSprint {
     public static final String MOD_ID = "toggle-sprint";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
-        System.out.println("Hello world!");
+
+    }
+
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {
+
     }
 }
